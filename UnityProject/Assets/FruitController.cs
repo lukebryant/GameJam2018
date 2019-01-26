edecd829adacf1e089858c07b78035ada2ef3af6
Ryutaro Ikeda <ryutaroikeda94@gmail.com>
2019-01-26 16:02:11 +0000

    make floaty ocean

--- UnityProject/Assets/FruitController.cs
+++ UnityProject/Assets/FruitController.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class FruitController : MonoBehaviour
 {
     Rigidbody2D body;
+    private bool inOcean = false;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Ocean")
+        {
+            Debug.Log("fruit in ocean");
+            inOcean = true;
+        }
+    }
     public void Drop()
     {
         body.gravityScale = 1f;
@@ -21,6 +30,14 @@ public class FruitController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (inOcean)
+        {
+            float r = Random.Range(0, 1);
+            if (r < 0.5) {
+                body.AddForce(new Vector2(-5, 0));
+            }else
+        }
+        */
     }
 }
