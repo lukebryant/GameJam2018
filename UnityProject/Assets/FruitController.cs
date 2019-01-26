d047933fbfa02728b1dc88879d0163462b472f56
Ryutaro Ikeda <ryutaroikeda94@gmail.com>
2019-01-26 13:50:30 +0000

    Add button for dropping fruit.
    Disable gravity on fruit initially.

---
+++ UnityProject/Assets/FruitController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitController : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    public void Drop()
+    {
+        body.gravityScale = 1f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
